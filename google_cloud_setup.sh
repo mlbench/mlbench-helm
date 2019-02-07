9ed135ae4b718739801cdd3dc799133a9328974a
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2019-02-07 12:12:12 +0100

    Adds fully functional Google Cloud deployment script

---
+++ google_cloud_setup.sh
@@ -0,0 +1,149 @@
+#!/bin/bash +x
+usage="usage: google_cloud_setup.sh <command>
+
+command:
+    get-credential  Get google credentials
+    create-cluster  Create a new cluster
+    install-chart   Install the Helm chart
+    upgrade-chart   Upgrade (Redeploy) the Helm chart
+    uninstall-chart Delete the Helm release/chart
+    delete-cluster  Delete cluster and perform a cleanup
+    help            Show this help"
+
+NUM_NODES=${NUM_NODES:-2}
+PREFIX=${PREFIX:-rel}
+RELEASE_NAME=${PREFIX}-${NUM_NODES}
+CLUSTER_NAME=${PREFIX}-${NUM_NODES}
+
+MACHINE_ZONE=europe-west1-b
+MYVALUES_FILE=myvalues.yaml
+
+MACHINE_TYPE=n1-standard-4
+CLUSTER_VERSION=1.10
+INSTANCE_DISK_SIZE=50
+DISK_TYPE=pd-standard
+DISK_SIZE=10GB
+GCE_PERSISTENT_DISK=gce-nfs-disk
+
+MACHINE_TYPE=`uname -m`
+HELM_DIR=/usr/local/bin/helm
+
+if [ ! -f $MYVALUES_FILE ]; then
+    echo "Custom Helm values yaml ($MYVALUES_FILE) not found"
+    exit 1
+fi
+
+if ! [ -x "$(command -v gcloud)" ]; then
+    echo "Installing Google Cloud SDK"
+
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+        curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-233.0.0-linux-x86_64.tar.gz | tar -xz
+    else
+        curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-233.0.0-linux-x86.tar.gz | tar -xz
+    fi
+
+    ./google-cloud-sdk/install.sh
+    ./google-cloud-sdk/bin/gcloud init
+fi
+
+if ! [ -x "$(command -v helm)" ]; then
+    echo "Installing Helm"
+
+    # curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
+    source <(curl -s https://raw.githubusercontent.com/helm/helm/master/scripts/get)
+fi
+
+function gcloud::get_credential(){
+    gcloud container clusters get-credentials --zone ${MACHINE_ZONE} ${CLUSTER_NAME}
+}
+
+function kube::worker::hostnames(){
+    # Get a sequence of names; if you want an array, use additional parentheses ($(kube::worker::hostnames))
+    kubectl get pods | grep 'worker' | awk '{print $1}'
+}
+
+function kube::worker::ips(){
+    # Get a sequence of names; if you want an array, use additional parentheses ($(kube::worker::hostnames))
+    kubectl get pods -o wide | grep worker | awk '{print $6}'
+}
+
+function chart::upgrade(){
+    # Install helm chart
+    helm upgrade --wait --recreate-pods -f ${MYVALUES_FILE} \
+        --timeout 900 --install ${RELEASE_NAME} . \
+        --set limits.workers=${NUM_NODES}
+}
+
+function join_by(){
+    local IFS="$1";
+    shift; echo "$*";
+}
+
+function gcloud::cleanup(){
+    gcloud compute firewall-rules delete --quiet ${CLUSTER_NAME}
+    gcloud container clusters delete --quiet --zone ${MACHINE_ZONE}  ${CLUSTER_NAME}
+}
+
+case $1 in
+    create-cluster)
+        # Create a CPU cluster
+        gcloud container clusters create ${CLUSTER_NAME} \
+            --zone=${MACHINE_ZONE} \
+            --cluster-version=${CLUSTER_VERSION} \
+            --enable-network-policy \
+            --machine-type=${MACHINE_TYPE} \
+            --num-nodes=${NUM_NODES} \
+            --disk-type=${DISK_TYPE} \
+            --disk-size=${INSTANCE_DISK_SIZE} \
+            --scopes=storage-full
+
+        # Get credential of the cluster
+        gcloud container clusters get-credentials --zone ${MACHINE_ZONE} ${CLUSTER_NAME}
+
+        kubectl --namespace kube-system create sa tiller
+
+        kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+
+        # Initialize helm to install charts
+        helm init --wait --service-account tiller
+        ;;
+
+    cleanup-cluster )
+        gcloud compute firewall-rules delete --quiet ${CLUSTER_NAME}
+        gcloud container clusters delete --quiet --zone ${MACHINE_ZONE}  ${CLUSTER_NAME}
+        ;;
+
+    install-chart)
+        chart::upgrade
+
+        # setup firewall
+        export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services ${RELEASE_NAME}-mlbench-master)
+        export NODE_IP=$(gcloud compute instances list|grep $(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}") |awk '{print $5}')
+        gcloud compute firewall-rules create --quiet ${CLUSTER_NAME} --allow tcp:$NODE_PORT,tcp:$NODE_PORT
+        ;;
+
+    upgrade-chart)
+        chart::upgrade
+        ;;
+
+
+    uninstall-chart)
+        helm delete --purge ${RELEASE_NAME}
+        gcloud compute firewall-rules delete --quiet ${CLUSTER_NAME}
+        ;;
+
+    delete-cluster)
+        gcloud::cleanup
+        ;;
+
+    get-credential)
+        gcloud::get_credential
+        ;;
+    help)
+        echo "$usage"
+        ;;
+    *)
+        printf "illegal option: %s\n" "$1" >&2
+        echo "$usage" >&2
+
+esac
